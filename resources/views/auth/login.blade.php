ab78286a5cb394e06058e40e6153abd45a8f3d03
Duc Cuong <duccuong.hust@gmail.com>
2015-09-02 19:58:56 +0800

    add view for authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,82 @@
-@extends('layouts.app')
-@section('meta-description', 'Studyhub - Login')
-@section('title', 'Login')
-@section('content')
-    <div class="row">
-		<div class="col-md-6 col-md-offset-3">
-            <div class="form-wrapper">
-                @if(session()->has('login_error'))
-                    <div class="alert alert-danger text-center">{!! session()->get('login_error') !!}</div>
-                @endif
-                {!! Form::open() !!}
-                    <div class="form-group">
-                        {!! Form::label('email', 'Email', ['class' => 'control-label']) !!}
-                        {!! Form::email('email', null, ['class' => 'form-control input-lg']) !!}
-                        {!! error_text($errors, 'email') !!}
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
-                        {!! Form::password('password', ['class' => 'form-control input-lg']) !!}
-                        {!! error_text($errors, 'password') !!}
-                    </div>
-                    <div class="form-group">
-                        <div class="checkbox">
-                            <label><input type="checkbox" name="remember"> Remember Me</label>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Đăng nhập | StudyHub - Hệ thống Hỗ trợ Học tập, Giảng dạy - Trường Đại học Bách Khoa Hà Nội</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-fixed-top navbar-dark bg-primary bg-faded">
+        <a href="{{ route('home') }}}" class="navbar-brand">StudyHub</a>
+        <ul class="nav navbar-nav pull-right">
+            <li class="nav-item">
+                <a href="{{ route('auth::register') }}" class="btn btn-success">Tạo tài khoản</a>
+            </li>
+        </ul>   
+    </nav>
+    <!-- End NAVBAR -->
+    <!-- CONTENT -->
+    <div class="wrapper" id="authPage">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="card auth-card">
+                        <div class="card-block">
+                            <h4 class="card-title">Đăng nhập</h4>
+                            @if (count($errors) > 0)
+                            <div class="alert alert-danger" role="alert">
+                                <p><strong>Oops!!</strong> Đã có lỗi xảy ra khi đăng nhập</p>
+                                <ul>
+                                @foreach($errors as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <!-- <h6 class="card-subtitle text-muted">Xin vui lòng điền thông tin đăng nhập</h6> -->
+                        </div>
+                        <div class="card-block">
+                            <!-- Login form -->
+                            <div class="form">
+                                <form action="{{ route('auth::login') }}" method="post">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <fieldset class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" name="email" placeholder="Email" required>
+                                    </fieldset>
+                                    <fieldset class="form-group">
+                                        <label for="password">Mật khẩu</label>
+                                        <input type="password" class="form-control" name="password" placeholder="Mật khẩu" required>
+                                    </fieldset>
+                                    <div class="checkbox">
+                                        <label class="c-input c-checkbox">
+                                            <input type="checkbox" name="remember">
+                                            <span class="c-indicator"></span> Ghi nhớ đăng nhập
+                                        </label>
+                                    </div>
+                                    <a href="#" class="card-link">Quên mật khẩu</a>
+                                    <button type="submit" class="btn btn-primary pull-right">Đăng nhập</button>
+                                    <div class="clearfix"></div>
+                                </form>
+                            </div>
+                            <!-- End Login form -->
                         </div>
                     </div>
-                    <div class="form-group">
-                        {!! Form::submit('Login', ['class' => 'btn btn-primary']) !!}
-                    </div>
-                {!! Form::close() !!}
-                <div class="form-link">
-                    Forgot Your Password? <a href="{{ url('auth/password/email') }}">Reset here</a><br>
-                    Don't have an account? <a href="{{ route('auth::register') }}"> Create a free account</a>
                 </div>
             </div>
-		</div>
-	</div>
-@stop
+        </div>
+    </div>
+    <!-- End CONTENT -->
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,38 +1,94 @@
-@extends('layouts.app')
-@section('meta-description', 'Studyhub - Register new account')
-@section('title', 'Registration')
-@section('content')
-	<div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="form-wrapper">
-                {!! Form::open() !!}
-                    <div class="form-group">
-                        {!! Form::label('name', 'Name', ['class' => 'control-label']) !!}
-                        {!! Form::text('name', null, ['class' => 'form-control input-lg']) !!}
-                        {!! error_text($errors, 'name') !!}
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Tạo tài khoản mới | StudyHub - Hệ thống Hỗ trợ Học tập, Giảng dạy - Trường Đại học Bách Khoa Hà Nội</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-fixed-top navbar-dark bg-primary bg-faded">
+        <a href="#" class="navbar-brand">StudyHub</a>
+        <ul class="nav navbar-nav pull-right">
+            <li class="nav-item">
+                <a href="{{ route('auth::login') }}" class="btn btn-success">Đăng nhập</a>
+            </li>
+        </ul>   
+    </nav>
+    <!-- End NAVBAR -->
+    <!-- CONTENT -->
+    <div class="wrapper" id="authPage">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="card auth-card">
+                        <div class="card-block">
+                            <h4 class="card-title">Tạo tài khoản mới</h4>
+                            <h6 class="card-subtitle text-muted">Xin vui lòng điền đầy đủ những thông tin dưới đây để tạo tài khoản mới</h6>
+                        </div>
+                        <div class="card-block">
+                            <!-- Signup form -->
+                            <form class="form" action="{{ route('auth::register') }}" method="post">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <!-- <fieldset class="form-group">
+                                    <label for="role">Bạn là</label>
+                                    <select name="role" id="roleSelect" class="form-control">
+                                        <option value="student">Sinh viên Đại học Bách Khoa Hà Nội</option>
+                                        <option value="lecturer">Giảng viên Đại học Bách Khoa Hà Nội</option>
+                                        <option value="staff">Cán bộ Đại học Bách Khoa Hà Nội</option>
+                                    </select>
+                                </fieldset>
+                                <fieldset class="form-group" id="studentIdField">
+                                    <label for="hustid">Mã số sinh viên</label>
+                                    <input type="text" class="form-control" name="hustid" placeholder="Mã số sinh viên">
+                                </fieldset> -->
+                                <fieldset class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" name="email" placeholder="Email">
+                                </fieldset>
+                                <fieldset class="form-group">
+                                    <label for="password">Mật khẩu</label>
+                                    <input type="password" class="form-control" name="password" placeholder="Mật khẩu">
+                                </fieldset>
+                                <fieldset class="form-group">
+                                    <label for="password_confirmation">Nhập lại mật khẩu</label>
+                                    <input type="password" class="form-control" name="password_confirmation" placeholder="Nhập lại mật khẩu">
+                                </fieldset>
+                                <div class="checkbox">
+                                    <label class="c-input c-checkbox">
+                                        <input type="checkbox" name="term_agree">
+                                        <span class="c-indicator"></span> Tôi đã đọc, hiểu và đồng ý với những <a href="#" class="card-link">Điều khoản sử dụng</a> của StudyHub
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary pull-right">Tạo tài khoản</button>
+                                <div class="clearfix"></div>
+                            </form>
+                            <!-- End Signup form -->
+                        </div>
                     </div>
-                    <div class="form-group">
-                        {!! Form::label('email', 'Email', ['class' => 'control-label']) !!}
-                        {!! Form::email('email', null, ['class' => 'form-control input-lg', 'placeholder' => 'username@example.com']) !!}
-                        {!! error_text($errors, 'email') !!}
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
-                        {!! Form::password('password', ['class' => 'form-control input-lg']) !!}
-                        {!! error_text($errors, 'password') !!}
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('password_confirmation', 'Password Confirmation', ['class' => 'control-label']) !!}
-                        {!! Form::password('password_confirmation', ['class' => 'form-control input-lg']) !!}
-                    </div>
-                    <div class="form-group">
-                        {!! Form::submit('Create Account', ['class' => 'btn btn-primary']) !!}
-                    </div>
-                {!! Form::close() !!}
-                <div class="form-link">
-                    Already had an account? <a href="{{ route('auth::login') }}"> Login here</a>
                 </div>
             </div>
         </div>
     </div>
-@stop
+    <!-- End CONTENT -->
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+    $('#roleSelect').change(function() {
+        var value = $(this).val();
+        if (value != 'student')
+            $('#studentIdField').css('display', 'none');
+        else
+            $('#studentIdField').css('display', 'block');
+    });
+    </script>
+</body>
+</html>
